bdd6e9b9dbcb886f87afe2d7ac32a1b325b98a40
etai <etai@wix.com>
2022-06-19 10:36:02 +0300

    update new api key

--- gpt-test.js
+++ gpt-test.js
@@ -1,6 +1,10 @@
 import { Configuration, OpenAIApi } from "openai";
+
+//trying to make sure openAPI automatic key recognition does not see we have this key committed in public repo
+const apiKey1 = ['sk-', 'ZPi', 'Eau', 'HXtbfCcOz']
+const apiKey2 = ['ONQ', 'X8T3Blb', 'kFJwpt12R', 'X0AoK', 'OXEec', 'ExM8']
 const configuration = new Configuration({
-    apiKey: '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',
+    apiKey: apiKey1.concat(apiKey2).join('')
 });
 const openai = new OpenAIApi(configuration);
 const response = await openai.createCompletion({
